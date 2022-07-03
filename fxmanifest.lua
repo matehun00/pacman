d63f1646bfc805c4d52e189587fad3627db38ce4
matehun00 <balogstudio@gmaail.com>
2022-07-03 13:05:57 +0200

    first commit

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'bodacious'
+games { 'common' }
+
+author { 'matehun00' }
+description { 'Pacman Loading screen' }
+version '0.0.1'
+
+loadscreen 'pacscreen.html'
+
+files {
+    'pacscreen.html',
+    'css/style.css',
+    'js/script.js',
+}
+--html pacman from onnayokheng
